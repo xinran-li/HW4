10a5a4b9d45512ea67be6369930fa20def0625c9
xinran-li <55333897+xinran-li@users.noreply.github.com>
2019-10-05 15:52:54 -0700

    Update faster-points.js

--- faster-points.js
+++ faster-points.js
@@ -1,1 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+  colorMode(HSB, width, height, 100);
+}
 
+function draw() {
+  var x = random(width);
+  var y = random(height);
+  
+  for (var a = 0; a < 100; a++) {
+    stroke(x, y, 100);
+    point(x, y)
+  }
+}
