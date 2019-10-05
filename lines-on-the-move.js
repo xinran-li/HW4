1aaa219d0359e1b2ba9a840cfb68596b6e1b9685
xinran-li <55333897+xinran-li@users.noreply.github.com>
2019-10-05 15:01:18 -0700

    Update lines-on-the-move.js

--- lines-on-the-move.js
+++ lines-on-the-move.js
@@ -1,1 +1,20 @@
 # HW4
+function setup() {
+  createCanvas(400, 400);
+  colorMode(HSB);
+}
+
+var shift = 0;
+
+function draw() {
+  background(255);
+
+  for (var x = 10; x < 500; x = x + 10) {
+    line(x+shift, height/2, x+100+shift, height/2-75);
+  }
+
+    shift = shift + 2;
+ 
+ if(shift > width)
+  shift = 0;
+}
